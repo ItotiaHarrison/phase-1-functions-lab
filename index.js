48603769eeca4093c62b4ae6adfba154b7c648d6
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-08-20 14:24:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const Headquarters = 42
+
+function distanceFromHqInBlocks(pickUp) {
+    const distanceInBlocks = Math.abs(pickUp - Headquarters)
+    return distanceInBlocks
+}
+
+function distanceFromHqInFeet(pickUp) {
+    let distanceInBlocks = distanceFromHqInBlocks(pickUp)
+    return distanceInBlocks * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const distanceTravelled = Math.abs(start - destination)
+    return distanceTravelled * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    let distanceInFeet = distanceTravelledInFeet(start, destination)
+    if (distanceInFeet < 400) {
+        return 0
+    } else if (distanceInFeet > 400 && distanceInFeet < 2000) {
+        return (distanceInFeet - 400) * 0.02
+    }
+    else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+        return  25
+    }
+    else {
+        return 'cannot travel that far'
+    }
+}
